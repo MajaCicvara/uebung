f7742d25998f9027ebbbe3a82349a256630fbf5a
Cicvara.Maja <Cicvara.Maja@SK-001C255EF7CA.2631.skool.net>
2018-09-17 15:34:37 +0200

    initial collatz now

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,8 +3,19 @@ public class HelloWorld {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World!");
-
+		int Zahl = 7;
+		while(Zahl>1)
+		{
+		if (Zahl % 2 != 0)
+		{
+		Zahl = Zahl*3+1;	
+		}
+		else
+		{
+		Zahl = Zahl/2;	
+		}
+		System.out.print(Zahl +", ");
+		}
 	}
 
 }
